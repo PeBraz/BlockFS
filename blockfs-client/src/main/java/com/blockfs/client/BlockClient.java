da2ffb381127aebd016f48c7cc22760f611d7977
pebraz <pbraz.93@gmail.com>
2016-03-07 17:04:57 +0000

    Adding some client tests and prototype for writing and reading

--- blockfs-client/src/main/java/com/blockfs/client/BlockClient.java
+++ blockfs-client/src/main/java/com/blockfs/client/BlockClient.java
@@ -1,8 +1,10 @@
 package com.blockfs.client;
 
 import java.io.*;
+import java.nio.file.Paths;
 import java.security.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.blockfs.server.utils.CryptoUtil;     //... takes from server
@@ -22,41 +24,28 @@ public class BlockClient implements IBlockClient{
         this.blockServer = new BlockServerRequests();
     }
 
-    public byte[] getPublic() {
-        return keys.getPublic().getEncoded();
-    }
+
 
     /**
      *  Initializes keys and serializes them to a file, future calls will use the old keys
+     *
      *  @return hash of current public key
      */
 
-    public String FS_init(){
-
-        if (new File(KEYS_FILE).exists()) {
-
-            try {
-                ObjectInputStream ois = new ObjectInputStream(new FileInputStream(KEYS_FILE));
-                keys = (KeyPair) ois.readObject();
-                ois.close();
-            } catch (IOException | ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-
-        }
-        else {
+    public String FS_init() {
+        if (! new File(KEYS_FILE).exists()) {
             try {
                 KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
                 keygen.initialize(1024);
                 keys = keygen.generateKeyPair();
-                ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream(KEYS_FILE));
-                ous.writeObject(keys);
-                ous.close();
-            } catch (IOException | NoSuchAlgorithmException e) {
+                saveKeyPair(keys);
+            } catch (NoSuchAlgorithmException e) {
                 e.printStackTrace();
             }
         }
-
+        else {
+                keys = loadKeyPair();
+        }
 
         return CryptoUtil.generateHash(keys.getPublic().getEncoded());
     }
@@ -65,51 +54,66 @@ public class BlockClient implements IBlockClient{
 
         if (keys == null) throw new UninitializedFSException();
 
+        if (size > contents.length)
+            size = contents.length;
+
         String id = CryptoUtil.generateHash(keys.getPublic().getEncoded());
         List<String> hashes = this.getPKB(id);
 
         int block_index = pos / BLOCK_SIZE;
 
-        //Creates empty blocks for difference between pos given and end of PKB
-        //e.g: if there are 5 blocks and block index is 7, need to add block 6 to server
-        if (block_index > hashes.size() + 1)
-            for (int i = hashes.size(); i < block_index - 1; i++ )
-                hashes.add(blockServer.put_h(new byte[BLOCK_SIZE]));
-
-        int contentsOffset = 0;
-        int datablockOffset = pos % BLOCK_SIZE;
-        while (size / BLOCK_SIZE > 0) {
-
-            //Gets an old BLOCK or creates a new BLOCK
-            byte[] dataBlock = (block_index < hashes.size()) ? this.getDB(hashes.get(block_index))
-                    : new byte[BLOCK_SIZE];
-
-            System.arraycopy(contents, contentsOffset, dataBlock, datablockOffset, BLOCK_SIZE - datablockOffset);
-            String new_hash = blockServer.put_h(dataBlock);
-
-            if (block_index < hashes.size())
-                hashes.set(block_index, new_hash);
-            else
-                hashes.add(new_hash);
-
-            size -= BLOCK_SIZE - datablockOffset;
-            contentsOffset += BLOCK_SIZE - datablockOffset;
-            datablockOffset = 0;
-            block_index++;
+        //previous last block needs to be fully padded
+        if (block_index == hashes.size()) {
+            byte[] old_block = blockServer.get(hashes.get(block_index - 1));
+            byte[] new_block = new byte[BLOCK_SIZE];
+            System.arraycopy(old_block, 0, new_block, 0, old_block.length);
+            hashes.set(block_index - 1, blockServer.put_h(new_block));
         }
 
-        byte[] dataBlock = (block_index < hashes.size()) ? this.getDB(hashes.get(block_index))
-                : new byte[BLOCK_SIZE];
+        //Creates empty blocks for difference between pos given and end of PKB
+        //e.g: if there are 5 blocks and block index is 7, need to add block 6 to server
+        //  (also need to pad block 5 completely)
+        if (block_index > hashes.size())
+            for (int i = hashes.size(); i < block_index; i++ )
+                hashes.add(blockServer.put_h(new byte[BLOCK_SIZE])); // adds a full padded block
 
-        System.arraycopy(contents, contentsOffset, dataBlock, datablockOffset, size % BLOCK_SIZE);
-        String new_hash = blockServer.put_h(dataBlock);
+        int contentsOffset = 0;
+        int startOffset = pos % BLOCK_SIZE;
+        while (contentsOffset < size) {
+
+            int length = Math.min(BLOCK_SIZE - startOffset, size - contentsOffset);
+            if (block_index < hashes.size()) {
+                byte[] old_block = this.getDB(hashes.get(block_index));
+                //new block can be bigger than old block
+                int new_block_length = Math.max(old_block.length, startOffset + length);
 
 
-        if (block_index < hashes.size())
-            hashes.set(block_index, new_hash);
-        else
-            hashes.add(new_hash);
+                byte[] new_block = new byte[new_block_length];    //need to cut excess size after
 
+                System.arraycopy(old_block, 0, new_block, 0, old_block.length);
+
+
+                //Add new content to block from position start
+                System.arraycopy(contents, contentsOffset, new_block, startOffset, length);
+
+                if (size - contentsOffset < old_block.length)
+                    System.arraycopy(old_block, startOffset + length, new_block, startOffset + length, old_block.length - (startOffset + length));
+
+                //fingers crossed
+                hashes.set(block_index, blockServer.put_h(new_block));
+            }
+            else {
+                byte[] new_block = new byte[startOffset + length];
+                System.arraycopy(contents, contentsOffset, new_block, 0, length);
+
+                hashes.add(blockServer.put_h(new_block));
+            }
+
+
+            contentsOffset += length;
+            startOffset = 0;
+            block_index++;
+        }
 
         this.putPKB(hashes, keys);
 
@@ -121,27 +125,29 @@ public class BlockClient implements IBlockClient{
             size = contents.length;
 
         List<String> hashes = this.getPKB(hash);
-
         int block_index = pos / BLOCK_SIZE;
 
         if (block_index >= hashes.size()) return 0;
 
+        int startOffset = pos % BLOCK_SIZE;
         int contentsOffset = 0;
-        int datablockOffset = pos % BLOCK_SIZE;
-        int initialSize = size;
-        while (block_index < hashes.size()) {
+        while (contentsOffset < size) {
 
-            //Gets an old BLOCK or creates a new BLOCK
             byte[] dataBlock = this.getDB(hashes.get(block_index));
 
-            System.arraycopy(dataBlock, datablockOffset, contents, contentsOffset, BLOCK_SIZE - datablockOffset);
+            /* Reading length depends:
+             *      - if starting to read from middle of block (and not beginning) (dataBlock.length - startOffset)
+             *      - if limited by size argument (size - contentsOffset)
+             *      - If limited by file size (reading from incomplete block) (dataBlock.length - startOffset)
+             */
+            int length = Math.min(dataBlock.length - startOffset, size - contentsOffset);
+            System.arraycopy(dataBlock, startOffset, contents, contentsOffset, length);
 
-            size -= BLOCK_SIZE - datablockOffset;
-            contentsOffset += BLOCK_SIZE - datablockOffset;
-            datablockOffset = 0;
+            contentsOffset += length;
+            startOffset = 0;
             block_index++;
         }
-        return initialSize - size;
+        return contentsOffset;
     }
 
 
@@ -194,6 +200,23 @@ public class BlockClient implements IBlockClient{
         return pkhash;
     }
 
+    private void saveKeyPair(KeyPair kp) {
+        try (ObjectOutputStream ous = new ObjectOutputStream(new FileOutputStream(KEYS_FILE))) {
+            ous.writeObject(kp);
+        } catch (IOException  e) {
+            e.printStackTrace();
+        }
+    }
+    private KeyPair loadKeyPair() {
+        KeyPair kp = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(KEYS_FILE))) {
+            kp = (KeyPair) ois.readObject();
+            ois.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return kp;
+    }
 
 
 }
--- blockfs-client/src/test/java/com/blockfs/client/BlockServerTest.java
+++ blockfs-client/src/test/java/com/blockfs/client/BlockServerTest.java
@@ -9,6 +9,10 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 
 /**
@@ -22,17 +26,16 @@ public class BlockServerTest
 
     private final IBlockServer server = new BlockFSService();
 
-
     //Test Client, calls server functions directly
     private final IBlockClient client = new BlockClient(new IBlockServerRequests() {
         @Override
         public byte[] get(String hash) {
-            return server.get(hash);
+
+             return server.get(hash);
         }
 
         @Override
         public String put_k(byte[] data, byte[] signature, byte[] pubKey) {
-
             try {
                 return server.put_k(data, signature, pubKey);
             } catch (WrongDataSignature wrongDataSignature) {
@@ -65,45 +68,209 @@ public class BlockServerTest
     public static Test suite()
     {
         return new TestSuite(BlockServerTest.class);
-        /*
-        TestSuite suite = new TestSuite();
-        suite.addTest(new BlockServerTest("testFSWriteFirstBlock"));
-        // .. suite.addTest(new BlockServerTest(   ....      ));
-        return suite;
-*/    }
+
+    }
 
     @Override
     public void setUp() {
+        BlockClient.BLOCK_SIZE = 4;
         File path = new File(BLOCK_DIR);
         if (path.exists())
-            path.delete();
-
-        path.mkdir();
+            for (File f : path.listFiles())
+                f.delete();
+        else
+            path.mkdir();
     }
 
     @Override
     public void tearDown() {
         File path = new File(BLOCK_DIR);
-        path.delete();
+        for (File f : path.listFiles())
+            f.delete();
     }
+
+    public void testPutH(){
+
+        byte[] data = "Hello world!".getBytes();
+
+        //creates a block with length data.length and returns the hash
+        String hash = server.put_h(data);
+
+        //hash returned is correct
+        assertEquals(hash, CryptoUtil.generateHash(data));
+
+
+        Path p = Paths.get(BLOCK_DIR, hash);
+        //file was created locally in the server
+        assertTrue(Files.exists(p));
+
+        try {
+            byte[] content = Files.readAllBytes(p);
+            //file content is the same as the original
+            assertEquals(new String(content), new String(data));
+        } catch (IOException e) {
+            fail();
+        }
+    }
+
+    public void testGetH() {
+
+        byte[] data = "Hello world!".getBytes();
+
+        byte[] content = server.get(server.put_h(data));
+
+        assertEquals(new String(data), new String(content));
+
+    }
+
 
     /**
      * Writes a new File:
      *  - will generate 2 files server side ( 1 data block & 1 public key block )
      *  - will pass integrity validation
+     *  - doesn't check content
      */
     public void testFSWriteFirstBlock()
     {
-        byte[] data = new byte[BlockClient.BLOCK_SIZE];
-        String key = client.FS_init();
+
+
+        BlockClient.BLOCK_SIZE = 4;
+
+        byte[] data = "Hello".getBytes();
+        byte[] buffer = new byte[8];
+        String pkhash = client.FS_init();
+
         try {
-            client.FS_write(0, BlockClient.BLOCK_SIZE, data);
+            client.FS_write(4, 4, data);
+            client.FS_read(pkhash, 4, 4, buffer);
         } catch (IBlockServerRequests.IntegrityException | IBlockClient.UninitializedFSException e) {
             fail();
-            return;
         }
-        assertEquals(new File("data").listFiles().length, 2);
-        assertTrue(new File("data", CryptoUtil.generateHash(data)).exists());
-        assertTrue(new File("data", CryptoUtil.generateHash(((BlockClient)client).getPublic())).exists());
+        assertEquals(new String(new byte[] {0,0,0,0,'H', 'e','l', 'l', 'o'}), new String(buffer));
     }
+    /**
+     *  Writes a new file with 3 blocks
+     *      - blocks 1 and 2 are the same, pkb should point twice to the same block
+     */
+
+    public void testFSWriteBlocks()
+    {
+        byte[] data = new byte[2050]; //3 blocks
+
+        String pkFilename = client.FS_init();
+        try {
+            client.FS_write(0, data.length, data);
+        } catch (IBlockServerRequests.IntegrityException | IBlockClient.UninitializedFSException e) {
+            fail();
+        }
+
+        String data1Filename = CryptoUtil.generateHash(new byte[1024]); //data block 2 is the same block (same hash)
+        String data3Filename = CryptoUtil.generateHash(new byte[2]);
+
+        //check if files were created successfully
+        assertEquals(new File("data").listFiles().length, 3);
+        assertTrue("data block1 " + data1Filename + " should exist ", new File("data", data1Filename).exists());
+        assertTrue("data block3 " + data1Filename + " should exist ", new File("data", data3Filename).exists());
+        assertTrue("Public key block " + pkFilename + " should exist", new File("data", pkFilename).exists());
+
+        //checks Public key file contents (locally at server side)
+        // (Won't work, because locally file is stored as json)
+        /*Path p = Paths.get("data", pkFilename);
+
+        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(Files.readAllBytes(p)))){
+            List<String> hashes = (List<String>)ois.readObject();
+            assertEquals(hashes, Arrays.asList( new String[] {data1Filename,data1Filename,data3Filename}));
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            fail();
+        }*/
+
+        // checks data block contents
+        Path p1 = Paths.get("data", data1Filename);
+        Path p3 = Paths.get("data", data3Filename);
+        try {
+            byte[] content = Files.readAllBytes(p1);
+            assertEquals(new String(content), new String(data));
+            content = Files.readAllBytes(p3);
+            assertEquals(new String(content), new String(new byte[2]));
+        } catch (IOException e) {
+            fail();
+        }
+
+    }
+
+    /**
+     *  Writes a new file with 3 blocks
+     *      - blocks 1 and 2 are the same, pkb should point twice to the same block
+     */
+    public void testFSWriteOverBlocks()
+    {
+        byte[] old_data = new byte[2050]; //3 blocks
+        byte[] new_data = "Hello World!".getBytes();
+        String pkFilename = client.FS_init();
+        try {
+            client.FS_write(0, old_data.length, old_data);
+        } catch (IBlockServerRequests.IntegrityException | IBlockClient.UninitializedFSException e) {
+            fail();
+        }
+
+        try {
+            client.FS_write(1020, new_data.length, new_data);
+        } catch (IBlockServerRequests.IntegrityException | IBlockClient.UninitializedFSException e) {
+            fail();
+        }
+
+
+        /*
+
+
+        String data1Filename = CryptoUtil.generateHash(new byte[1024]); //data block 2 is the same block (same hash)
+        String data3Filename = CryptoUtil.generateHash(new byte[2]);
+
+        //check if files were created successfully
+        assertEquals(new File("data").listFiles().length, 3);
+        assertTrue("data block1 " + data1Filename + " should exist ", new File("data", data1Filename).exists());
+        assertTrue("data block3 " + data1Filename + " should exist ", new File("data", data3Filename).exists());
+        assertTrue("Public key block " + pkFilename + " should exist", new File("data", pkFilename).exists());
+*/
+        //checks Public key file contents (locally at server side)
+        // (Won't work, because locally file is stored as json)
+        /*Path p = Paths.get("data", pkFilename);
+
+        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(Files.readAllBytes(p)))){
+            List<String> hashes = (List<String>)ois.readObject();
+            assertEquals(hashes, Arrays.asList( new String[] {data1Filename,data1Filename,data3Filename}));
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            fail();
+        }*/
+
+        // checks data block contents
+      /*  Path p1 = Paths.get("data", data1Filename);
+        Path p3 = Paths.get("data", data3Filename);
+        try {
+            byte[] content = Files.readAllBytes(p1);
+            assertEquals(new String(content), new String(data));
+            content = Files.readAllBytes(p3);
+            assertEquals(new String(content), new String(new byte[2]));
+        } catch (IOException e) {
+            fail();
+        }
+*/
+    }
+
+/*
+    public void testFSRead() {
+
+        String data = "Hello world!";
+        byte[] buffer = new byte[BlockClient.BLOCK_SIZE];
+        String key = client.FS_init();
+        try {
+            client.FS_write(0, data.length(), data.getBytes());   //create my file
+            client.FS_read(key, 0, data.length(), buffer); //read my file
+        } catch (IBlockServerRequests.IntegrityException | IBlockClient.UninitializedFSException e) {
+            fail();
+        }
+        assertEquals(data, new String(buffer));
+    }*/
 }
