77026963555d35add419a892b54a24226e8d091b
Nuno Nogueira <nunofmn@gmail.com>
2016-05-05 17:56:27 +0100

    Add HTTP request logic to ConnectionPool read operation.

--- blockfs-client/src/main/java/com/blockfs/client/connection/ConnectionPool.java
+++ blockfs-client/src/main/java/com/blockfs/client/connection/ConnectionPool.java
@@ -1,18 +1,23 @@
 package com.blockfs.client.connection;
 
+import com.blockfs.client.exception.ServerRespondedErrorException;
+import com.blockfs.client.rest.RestClient;
+import com.blockfs.client.rest.model.Block;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ConnectionPool {
-    private final int QUORUMSIZE = 5;
+    private int QUORUMSIZE;
     private List<String> nodes;
     private ExecutorService executor;
 
     public ConnectionPool(List<String> nodes) {
         this.nodes = nodes;
         this.executor = Executors.newFixedThreadPool(nodes.size());
+        this.QUORUMSIZE = (nodes.size()/2) + 1;
     }
 
     public ConnectionPool() {
@@ -23,33 +28,41 @@ public class ConnectionPool {
         this.nodes.add(node);
     }
 
-    public void read(String id) {
+    public Block read(final String id) throws ServerRespondedErrorException{
         AtomicInteger count = new AtomicInteger();
 
-        CompletionService<String> completionService = new ExecutorCompletionService<String>(executor);
+        CompletionService<Block> completionService = new ExecutorCompletionService<Block>(executor);
+
+        //TODO: Optimization for content blocks
+        //TODO: Timestamp verification
 
         for(final String node : this.nodes) {
-            completionService.submit(new Callable<String>() {
+            completionService.submit(new Callable<Block>() {
                 @Override
-                public String call() throws Exception {
-                    return "Node URL: " + node;
+                public Block call() throws Exception {
+                    Block block = RestClient.GET(id, node);
+                    return block;
                 }
             });
         }
 
         try {
 
-        while(count.get() < QUORUMSIZE) {
-            Future<String> future = completionService.take();
-            count.incrementAndGet();
+            List<Block> received = new LinkedList<Block>();
 
-            System.out.println(future.get());
-        }
+            while(count.get() < QUORUMSIZE) {
+                Future<Block> future = completionService.take();
+                count.incrementAndGet();
+
+                received.add(future.get());
+            }
+
+            return received.get(0);
 
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            throw new ServerRespondedErrorException();
         } catch (ExecutionException e) {
-            e.printStackTrace();
+            throw new ServerRespondedErrorException();
         }
 
     }
