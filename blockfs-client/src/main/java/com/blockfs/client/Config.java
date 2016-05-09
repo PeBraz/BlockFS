a3881d9a4808e408f011aa3b69b0d9735b8d515b
Nuno Nogueira <nunofmn@gmail.com>
2016-05-09 14:23:42 +0100

    Add static initialization of servers endpoints.

--- blockfs-client/src/main/java/com/blockfs/client/Config.java
+++ blockfs-client/src/main/java/com/blockfs/client/Config.java
@@ -1,10 +1,19 @@
 package com.blockfs.client;
 
-/**
- * Created by Sampaio on 25/04/2016.
- */
+import java.util.HashMap;
+import java.util.Map;
+
 public class Config {
-    public final static String[] ENDPOINTS = {"http://0.0.0.0:5050/", "http://0.0.0.0:5051/", "http://0.0.0.0:5052/"};
+    public final static Map<String, String> ENDPOINTS;
+
+    static {
+        ENDPOINTS = new HashMap<String, String>();
+        ENDPOINTS.put("http://0.0.0.0:5050/", "secret1");
+        ENDPOINTS.put("http://0.0.0.0:5051/", "secret2");
+        ENDPOINTS.put("http://0.0.0.0:5052/", "secret3");
+        ENDPOINTS.put("http://0.0.0.0:5053/", "secret4");
+        ENDPOINTS.put("http://0.0.0.0:5054/", "secret5");
+    };
 
     public final static boolean enableCardTests = false;
 
